57974621c25cdf890a1b7b95be569fc63c36fd73
richard koopmann <email@rkoopmann.com>
2021-03-24 11:33:48 -0500

    helper scripts

---
+++ syntaxes/dev.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp grammar-ledger\ time.json ~/Library/Mobile\ Documents/iCloud\~com\~agiletortoise\~Drafts5/Documents/Library/Syntaxes/
+
+bbedit ~/Library/Mobile\ Documents/iCloud\~com\~agiletortoise\~Drafts5/Documents/Library/Syntaxes/grammar-ledger\ time.json
---
+++ syntaxes/push.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cp ~/Library/Mobile\ Documents/iCloud\~com\~agiletortoise\~Drafts5/Documents/Library/Syntaxes/grammar-ledger\ time.json .
